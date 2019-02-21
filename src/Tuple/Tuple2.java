d1196c0b1d668cb3c7f6aa093ab99d9f3f488bc0
t-he-name <miversen33@gmail.com>
2019-02-20 19:49:25 -0600

    Cleaned up toString override

--- src/Tuple/Tuple2.java
+++ src/Tuple/Tuple2.java
@@ -22,6 +22,6 @@ public class Tuple2<First, Second> {
 
     @Override
     public String toString() {
-        return "("+first.toString()+","+second.toString()+")";
+        return "("+first.toString()+" , "+second.toString()+")";
     }
 }
