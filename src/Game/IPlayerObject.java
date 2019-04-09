4d260a5979d09c6613179bb2e37a8902d5e8d2dc
t-he-name <miversen33@gmail.com>
2019-04-08 19:04:18 -0500

    Added ability to retrieve movement state directly from IPlayerObjects instead of chaining funtion calls.

--- src/Game/IPlayerObject.java
+++ src/Game/IPlayerObject.java
@@ -2,6 +2,7 @@ package Game;
 
 import Game.Field.Endzone;
 import Game.GamePlay.GamePlayer;
+import PhysicsEngine.Movements.MovementAction;
 import PhysicsEngine.Movements.MovementEngine;
 
 public interface IPlayerObject {
@@ -10,6 +11,7 @@ public interface IPlayerObject {
     String getOwnerHash();
     String getPlayerHash();
     PlayerState getPlayerState();
+    PlayerState getMovementState();
     void calculateMove();
     void setPlayerState(MovementEngine engine, PlayerState state);
     boolean sameTeamCheck(GamePlayer comparePlayer);
