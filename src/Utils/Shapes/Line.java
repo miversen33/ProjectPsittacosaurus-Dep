10ecd3f480850f1c457fe02cdc1027db6f4dac61
t-he-name <miversen33@gmail.com>
2019-04-08 19:10:19 -0500

    Corrected Intersection issue on Lines where collision points where found outside start/end points.

--- src/Utils/Shapes/Line.java
+++ src/Utils/Shapes/Line.java
@@ -1,15 +1,14 @@
-package PhysicsEngine.PhysicsObjects;
+package Utils.Shapes;
 
+import Utils.PhysicsObjects.Vector;
 import Tuple.Tuple2;
 import Utils.Location;
 
-/**
- * Is this really the best place to do this?
- * TODO
- */
-final class Line{
+public final class Line{
     private final Tuple2<Double, Double> startingPoint;
     private final Tuple2<Double, Double> endingPoint;
+    private final double slope;
+    private final double yInt;
 
     private enum Orientation{
         COUNTERCLOCKWISE,
@@ -23,61 +22,77 @@ final class Line{
         if(slopeDiff == 0) return Orientation.COLLINEAR;
         return slopeDiff > 0 ? Orientation.CLOCKWISE : Orientation.COUNTERCLOCKWISE;
     }
-    
-    Line(final Tuple2<Double, Double> start, final Tuple2<Double, Double> end){
+
+    public Line(final Tuple2<Double, Double> start, final Tuple2<Double, Double> end){
         startingPoint = start;
         endingPoint = end;
+        slope = (endingPoint.getSecond() - startingPoint.getSecond()) / (endingPoint.getFirst() - startingPoint.getFirst());
+        yInt = startingPoint.getSecond() - (slope * startingPoint.getFirst());
     }
 
-    final Tuple2<Double, Double> doesIntersect(final Line line) {
+    public Line(final Tuple2<Double, Double> start, final Vector move){
+        this(start, new Tuple2<>(start.getFirst() + move.getChangeX(), start.getSecond() + move.getChangeY()));
+    }
+//
+//    public Line(final Tuple2<Double, Double> start, final double slope){
+//        startingPoint = start;
+//        endingPoint = null;
+//        this.slope = slope;
+//        yInt = startingPoint.getSecond() - (slope * startingPoint.getFirst());
+//    }
+
+    public final Tuple2<Double, Double> getStartingPoint(){
+        return startingPoint;
+    }
+
+    public final double getSlope(){
+        return slope;
+    }
+
+    public final double getYInt(){
+        return yInt;
+    }
+
+    public final boolean containsPoint(final Tuple2<Double, Double> point){
+        return
+               ((startingPoint.getFirst() <= point.getFirst() && point.getFirst() <= endingPoint.getFirst()) ||
+               (endingPoint.getFirst() <= point.getFirst() && point.getFirst() <= startingPoint.getFirst()))
+                                                    &&
+               ((startingPoint.getSecond() <= point.getSecond() && point.getSecond() <= endingPoint.getSecond()) ||
+               (endingPoint.getSecond() <= point.getSecond() && point.getSecond() <= startingPoint.getSecond()))
+        ;
+    }
+
+    public final Tuple2<Double, Double> doesIntersect(final Line line) {
         final Orientation firstLineFirstHalf = FindOrientation(startingPoint, endingPoint, line.endingPoint);
         final Orientation firstLineSecondHalf = FindOrientation(startingPoint, endingPoint, line.startingPoint);
         final Orientation secondLineFirstHalf = FindOrientation(line.startingPoint, line.endingPoint, startingPoint);
         final Orientation secondLineSecondHalf = FindOrientation(line.startingPoint, line.endingPoint, endingPoint);
 
-        double intX;
-        double intY;
-
         if (firstLineFirstHalf == Orientation.COLLINEAR &&
                 firstLineSecondHalf == Orientation.COLLINEAR &&
                 secondLineFirstHalf == Orientation.COLLINEAR &&
                 secondLineSecondHalf == Orientation.COLLINEAR) {
-            final Tuple2<Double, Double> point = handleCollinearFirstIntersection(line);
-            intX = point.getFirst();
-            intY = point.getSecond();
+            return handleCollinearFirstIntersection(line);
 
         } else {
 
-            double a1 = endingPoint.getSecond() - startingPoint.getSecond();
-            double b1 = startingPoint.getFirst() - endingPoint.getFirst();
-            double c1 = a1 * startingPoint.getFirst() + b1 * startingPoint.getSecond();
-            double a2 = line.endingPoint.getSecond() - line.startingPoint.getSecond();
-            double b2 = line.startingPoint.getFirst() - line.endingPoint.getFirst();
-            double c2 = a2 * line.startingPoint.getFirst() + b2 * line.startingPoint.getSecond();
+            Double x = (line.getYInt() - getYInt())/(getSlope() - line.getSlope());
+            Double y = ((line.getSlope()*getYInt()) - (getSlope()*line.getYInt()))/(line.getSlope()-getSlope());
+            final Tuple2<Double, Double> interceptPoint = new Tuple2<>(x,y);
 
-            double denom = a1 * b2 - a2 * b2;
-
-            intX = (b2 * c1 - b1 * c2) / denom;
-            intY = (a1 * c2 - a2 * c1) / denom;
+            if((x.isNaN() || x.isInfinite() || y.isNaN() || y.isInfinite()) ||
+               (!line.containsPoint(interceptPoint) || !containsPoint(interceptPoint))) return null;
+            return interceptPoint;
         }
-        boolean x1 = (((intX <= startingPoint.getFirst() && intX >= endingPoint.getFirst())) &&
-                ((intX >= startingPoint.getFirst() && intX <= endingPoint.getFirst()))) && (((intY <= startingPoint.getSecond() && intY >= endingPoint.getSecond())) &&
-                ((intY >= startingPoint.getSecond() && intY <= endingPoint.getSecond())));
-
-        if ((((intX <= startingPoint.getFirst() && intX >= endingPoint.getFirst())) &&
-             ((intX >= startingPoint.getFirst() && intX <= endingPoint.getFirst()))) &&
-            (((intY <= startingPoint.getSecond() && intY >= endingPoint.getSecond())) &&
-             ((intY >= startingPoint.getSecond() && intY <= endingPoint.getSecond())))){
-            return new Tuple2<>(intX, intY);
-        }
-//
-        return null;
     }
 
     private final Tuple2<Double, Double> handleCollinearFirstIntersection(final Line line){
         Vector v = new Vector(startingPoint, endingPoint);
         double d = Location.GetDistance(startingPoint, line.startingPoint);
         v = v.scale((d/2) / v.getMagnitude());
-        return new Tuple2<>(startingPoint.getFirst() + v.getChangeX(), startingPoint.getSecond() + v.getChangeY());
+        final Tuple2<Double, Double> interceptPoint = new Tuple2<>(startingPoint.getFirst() + v.getChangeX(), startingPoint.getSecond() + v.getChangeY());
+        if(containsPoint(interceptPoint) && line.containsPoint(interceptPoint)) return interceptPoint;
+        return null;
     }
 }
