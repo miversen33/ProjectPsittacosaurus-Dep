52768bc9124f1625a5a71f88b0f2ee0e42a99003
t-he-name <miversen33@gmail.com>
2019-04-10 01:14:58 -0500

    Deprecated CommonScalarTypes. This will likely be removed quickly as its not used

--- src/Utils/PhysicsObjects/CommonScalarTypes.java
+++ src/Utils/PhysicsObjects/CommonScalarTypes.java
@@ -1,5 +1,6 @@
 package Utils.PhysicsObjects;
 
+@Deprecated
 public interface CommonScalarTypes {
     public final static String MASS = "MASS";
     public final static String ACCELERATION = "ACCELERATION";
