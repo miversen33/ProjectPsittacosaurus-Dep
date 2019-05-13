8312e458a6858fa023848cb41b53d9b929aeedb5
Mike <miversen33@gmail.com>
2019-05-12 23:59:23 -0500

    Removed Unused Classes and Types
    Removed ScalarTypes from PhysicsObjects package as they were unnecessary and unused.

--- src/Utils/PhysicsObjects/CommonScalarTypes.java
+++
@@ -1,7 +0,0 @@
-package Utils.PhysicsObjects;
-
-@Deprecated
-public interface CommonScalarTypes {
-    public final static String MASS = "MASS";
-    public final static String ACCELERATION = "ACCELERATION";
-}
