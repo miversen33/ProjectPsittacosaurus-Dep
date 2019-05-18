2284f89fe815dd209c33338152f2d3a1f4861c99
Mike <miversen33@gmail.com>
2019-05-18 00:25:06 -0500

    Created a few new XML Strings

--- src/Utils/XML/XMLStrings.java
+++ src/Utils/XML/XMLStrings.java
@@ -4,8 +4,9 @@ public interface XMLStrings {
     String BEGIN_BRACKET = "<";
     String BEGIN_COMMENT = "<!--";
     String END_COMMENT = "-->";
-    String END_BRACKET = ">";
-    String CLOSE_BRACKET = "</";
+    String OPEN_END_BRACKET = ">";
+    String CLOSE_BRACKET_WITH_TITLE = "</";
+    String CLOSE_BRACKET = "/>";
     String QUOTATION = "\"";
     String EQUALS = "=";
     String INDENTATION = "    ";
